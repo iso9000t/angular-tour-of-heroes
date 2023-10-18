66164f234964645d625d995e0ac5448408308f98
iso9000t <sasha.gr81@gmail.com>
2023-10-18 01:39:49 -0300

    feat: add step 5 of the tutorial

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';// <-- NgModel lives
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
- 
-  heroes: Hero[] = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroSevice: HeroService) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-   }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
+  getHeroes(): void {
+    this.heroSevice.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 }
